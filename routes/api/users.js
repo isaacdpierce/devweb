d804ed3aab184c64e51e8af36ed8a84bb7e6a83b
Isaac Pierce <contact@isaacdpierce.com>
2018-07-29 19:40:52 -0700

    Config register and login validations. Profile Schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -17,12 +25,23 @@ router.get('/test', (req, res) => res.json({
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({
     email: req.body.email
   }).then(user => {
     if (user) {
+      errors.email = "Email already exists";
       return res.status(400).json({
-        email: 'Email already exists'
+        email: errors
       });
     } else {
       const avatar = gravatar.url(req.body.email, {
@@ -51,5 +70,81 @@ router.post('/register', (req, res) => {
     }
   });
 });
+
+// @route   GET api/users/login
+// @desc    Login user / return json web token(JWT)
+// @access  Public
+router.post('/login', (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({
+      email
+    })
+    .then(user => {
+      // Check for user
+      if (!user) {
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+      }
+
+      // Check password
+      bcrypt.compare(password, user.password) // bcrypt compares users entered pw to database hashed pw
+        .then(isMatch => {
+          if (isMatch) {
+
+            // User matched -> Create jwt payload
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            }
+            // Sign token
+            jwt.sign(
+              payload,
+              keys.secretOrKey, {
+                expiresIn: 3600
+              },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
+          } else {
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
+          }
+        });
+    });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  '/current',
+  passport.authenticate(
+    'jwt', {
+      session: false
+    }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport'); // authentication module
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -23,7 +24,11 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello!'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config - jwt Strategy 
+require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users', users);
